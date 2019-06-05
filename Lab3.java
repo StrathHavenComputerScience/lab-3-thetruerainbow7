44ecf51f2aedd44e33ea81662e33d87f1062e989
katiemclead <6362326+katiemclead@users.noreply.github.com>
2019-06-05 11:45:49 -0400

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,117 @@
+public class Lab3
+{
+  public static void testLightCandles1()
+  {
+    Robot.load("candles1.txt");
+    Robot.setDelay(0.05);
+    lightCandles();
+  }
+  
+  public static void testLightCandles2()
+  {
+    Robot.load("candles2.txt");
+    Robot.setDelay(0.05);
+    lightCandles();
+  }
+    
+  public static void lightCandles()
+  {
+    //insert instructions below
+  
+  
+  
+  }
+  
+  //Run this method to test completeRoom on map room1.txt
+  public static void testCompleteRoom1()
+  {
+     Robot.load("room1.txt");
+     Robot.setDelay(0.05);
+     completeRoom();
+  }
+  
+  //Run this method to test completeRoom on map room2.txt
+  public static void testCompleteRoom2()
+  {
+    Robot.load("room2.txt");
+    Robot.setDelay(0.05);
+    completeRoom();
+  }
+  
+  //Complete this method.  You will need to write additional helper methods.
+  public static void completeRoom()
+  {
+    //insert instructions below
+  
+  
+  
+  }
+  
+  //Run this method to test swapAll on map swap1.txt
+  public static void testSwapAll1()
+  {
+    Robot.load("swap1.txt");
+    Robot.setDelay(0.05);
+    swapAll();
+  }
+  
+  //Run this method to test swapAll on map swap2.txt
+  public static void testSwapAll2()
+  {
+    Robot.load("swap2.txt");
+    Robot.setDelay(0.05);
+    swapAll();
+  }
+
+  //Complete this method.  You will need to write additional helper methods.
+  public static void swapAll()
+  {
+    //insert instructions below
+  
+  
+  
+  }
+  
+  //Don't run these. I will!
+  public static void testLightCandles3()
+  {
+    Robot.load("candles3.txt");
+    Robot.setDelay(0.05);
+    lightCandles();
+  }
+  
+  public static void testLightCandles4()
+  {
+    Robot.load("candles4.txt");
+    Robot.setDelay(0.05);
+    lightCandles();
+  }
+  public static void testCompleteRoom3()
+  {
+     Robot.load("room3.txt");
+     Robot.setDelay(0.05);
+     completeRoom();
+  }
+  
+  public static void testCompleteRoom4()
+  {
+    Robot.load("room4.txt");
+    Robot.setDelay(0.05);
+    completeRoom();
+  }
+  
+  public static void testSwapAll3()
+  {
+    Robot.load("swap3.txt");
+    Robot.setDelay(0.05);
+    swapAll();
+  }
+  
+  //Run this method to test swapAll on map swap2.txt
+  public static void testSwapAll4()
+  {
+    Robot.load("swap4.txt");
+    Robot.setDelay(0.05);
+    swapAll();
+  }
+}
